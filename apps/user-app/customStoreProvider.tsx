256b6bf92819e58fc84f6e1297e201c4fc7f8e5b
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-15 23:03:02 +0530

    added next auth session provider

--- apps/user-app/customStoreProvider.tsx
+++ apps/user-app/customStoreProvider.tsx
@@ -1,16 +1,20 @@
 "use client"
 
-//custom client side provider for redux store to wrap the children of server side component"
+//custom client side provider for redux store and next auth session provider to wrap the children of server side component"
 
 import { ReactNode } from 'react';
 import { Provider } from "react-redux"
+import { SessionProvider } from 'next-auth/react';
 import Store from "@repo/store/store"
 
 const customStoreProvider = ({children}:{children:ReactNode}) => {
   return (
+    <SessionProvider>
         <Provider store={Store}>
             {children}
         </Provider>
+    </SessionProvider>
+
   )
 }
 
