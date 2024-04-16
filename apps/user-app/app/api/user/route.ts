43b2285f985796e44a61041a71c79bdba58258ff
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-16 14:18:57 +0530

    err handling

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,12 +3,16 @@ import { authOptions } from "../../lib/auth"
 import { NextResponse } from "next/server";
 
 export const GET= async()=>{
+   try{
     const session = await getServerSession(authOptions);
     if(session.user){
         return NextResponse.json({
             user:session.user
         });
-    }
+   }}
+   catch(e){
+    console.log("err while getting server session",e);
+   }
     return NextResponse.json({
         mess:"You are not logged in "
     }),{
