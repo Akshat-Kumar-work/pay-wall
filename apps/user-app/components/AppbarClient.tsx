7dfaba9f27b19ef15de497f8693114a470545fe3
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-16 13:45:16 +0530

    appbar client added

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { useSession } from "next-auth/react"
-import { signOut,signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { signOut, signIn } from "next-auth/react";
 import { Appbar } from "@repo/ui/appbar";
 import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
@@ -9,24 +9,28 @@ import { setUser } from "@repo/store/userSlice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-export function AppbarClient(){
-    const session = useSession();
-    const router = useRouter();
-    const dispatch = useDispatch();
-  const {user} = useSelector((state:any)=>state.user);
+export function AppbarClient() {
+  const session = useSession();
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state: any) => state.user);
 
- 
-  useEffect(()=>{
-   dispatch(setUser(session.data?.user));
-   console.log('session user',session.data?.user)
-   console.log('redux user',user)
-  },[session.data?.user])
+  useEffect(() => {
+    dispatch(setUser(session.data?.user));
+    console.log("session user", session.data?.user);
+    console.log("redux user", user);
+  }, [session.data?.user]);
 
-
-    return(
-        <div>
-            <Appbar  onSignin={()=>signIn()} onSignout={ async ()=>{ await signOut(); router.push("api/auth/signin")} } user={session.data?.user}  />
-            <div>user redux {user?.name}</div>
-        </div>
-    )
+  return (
+    <div>
+      <Appbar
+        onSignin={() => signIn()}
+        onSignout={async () => {
+          await signOut();
+          router.push("api/auth/signin");
+        }}
+        user={session.data?.user}
+      />
+    </div>
+  );
 }
