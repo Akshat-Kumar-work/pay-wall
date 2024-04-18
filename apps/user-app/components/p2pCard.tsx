8f8eac910ecef4ed65a4592c97d21a2d47502293
Akshat <iamakku0.0.0.1@gmail.com>
2024-04-18 13:43:20 +0530

    p2p transfer money card

---
+++ apps/user-app/components/p2pCard.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Card } from "@repo/ui/card";
+import { TextInput } from "@repo/ui/textInput";
+import { Button } from "@repo/ui/button";
+import { useState } from "react";
+import { transfer } from "../app/lib/p2pAction";
+
+const p2pCard = () => {
+  const [toemail, setEmail] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <div className=" w-full flex p-4">
+      <Card title="Send Money from Your Wallet">
+        <TextInput
+          label="email"
+          placeholder="akshat@gmail.com"
+          onChange={(email) => setEmail(email)}
+        />
+
+        <TextInput
+          label="Amount"
+          placeholder=""
+          onChange={(amount) => setAmount(Number(amount))}
+        />
+
+        <div className=" p-3">
+          <Button
+            onClick={async () => {
+              await transfer(toemail, amount * 100);
+             location.reload();
+            }} >
+            Transfer
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default p2pCard;
